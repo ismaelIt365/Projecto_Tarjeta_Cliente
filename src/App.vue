f5aef3ca677cf976724256b4d9684d341eb35df0
ismaelIt365 <132374212+ismaelIt365@users.noreply.github.com>
2023-05-12 17:08:49 +0200

    12/05/2023 Registro,Ventajas, QR

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <a v-if="$store.state.email" @click="logout">Logout</a>
+  </nav>
+  <router-view />
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +12,36 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+img {
+  width: 300px;
+  height: 200px;
 }
 </style>
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.commit('logout')
+    }
+  },
+  mounted() {
+    this.$store.commit('initalizeStore')
+  }
+}
+
+</script>
