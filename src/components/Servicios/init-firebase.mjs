35244e5cbcdee4ff2d228813c6f4811f7bdabd6d
Ismael <imariduena@365obrador.com>
2025-09-02 12:57:37 +0200

    update

--- src/components/Servicios/init-firebase.mjs
+++ src/components/Servicios/init-firebase.mjs
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBEuQUOzbbN_07PYsrWAz2JIzFxfrbzWKU",
-  authDomain: "club365obrador.firebaseapp.com",
-  projectId: "club365obrador",
-  storageBucket: "club365obrador.appspot.com",
-  messagingSenderId: "948532793638",
-  appId: "1:948532793638:web:4b4a558f120660662831e7",
-  measurementId: "G-4SMD59E0DC",
+  apiKey: "AIzaSyCz2jjNZ4xfmTj-_IuXMDOQ2Sn58_3pWuY",
+  authDomain: "club365obrador-ea09e.firebaseapp.com",
+  projectId: "club365obrador-ea09e",
+  storageBucket: "club365obrador-ea09e.firebasestorage.app",
+  messagingSenderId: "760392871474",
+  appId: "1:760392871474:web:e6aad700ad7f71a28f962b",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
